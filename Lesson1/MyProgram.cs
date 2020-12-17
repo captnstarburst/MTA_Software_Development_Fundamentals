9b763a3cae4ecc926290a4d28e48d5fecf4e63c1
captnstarburst <captnstarburst@gmail.com>
2020-12-17 17:16:04 -0500

    lesson 1, hello world

---
+++ Lesson1/MyProgram.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;dot
+using System.Threading.Tasks;
+
+namespace MyFirstProgram{
+    class Program{
+        static void Main(string[] args){
+            Console.WriteLine("This is my first program in C#");
+            Console.WriteLine("One of many to come");
+            Console.WriteLine("Please press the enter key: ");
+            Console.ReadLine();
+        }
+    }
+}
