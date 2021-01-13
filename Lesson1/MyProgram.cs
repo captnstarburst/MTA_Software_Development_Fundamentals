b665e3c9c6d42c536e8f0e58fba21f9467b80f9a
captnstarburst <captnstarburst@gmail.com>
2021-01-13 14:03:35 -0500

    notes

--- Lesson1/MyProgram.cs
+++ Lesson1/MyProgram.cs
@@ -4,9 +4,24 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace MyFirstProgram{
-    class Program{
-        static void Main(string[] args){
+namespace MyFirstProgram
+{
+    /* 
+        namespaces are used to organize classes 
+        they uniquely identify classes
+        namespaces and class name are combined for the fully qualified class name
+        each fully qualified name should be unique
+    */
+
+    class Program
+    {
+        /*
+            classes are organized in namespaces
+            best practice is to have one class per file
+        */
+        static void Main(string[] args)
+        {
+            // Semi colons are not optional in C#
             Console.WriteLine("This is my first program in C#");
             Console.WriteLine("One of many to come");
             Console.WriteLine("Please press the enter key: ");
@@ -14,3 +29,5 @@ namespace MyFirstProgram{
         }
     }
 }
+
+//dotnet run in the terminal
